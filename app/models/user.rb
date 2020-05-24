b6505c263c6bc37ea468a15d2716e65bbf211197
hatake10969 <kawabata1096@gmail.com>
2020-05-24 23:50:24 +0900

    Testing user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,15 @@ class User < ApplicationRecord
         :recoverable, :rememberable, :validatable,
         :confirmable, :lockable
 
-  validates :nickname,       presence: true
-  validates :last_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+$\z/ }
-  validates :first_name,     presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+$\z/ }
-  validates :lastname_kana,  presence: true, format: { with: /\A([ァ-ン]|ー)+$\z/ }
-  validates :firstname_kana, presence: true, format: { with: /\A([ァ-ン]|ー)+$\z/ }
-  validates :birth_day,      presence: true
-  validates :email,          presence: true, uniqueness: true, format: { with: /\A[a-zA-Z0-9_\#!$%&`'*+\-{|}~^\/=?\.]+@[a-zA-Z0-9][a-zA-Z0-9\.-]+\z/ }
-  validates :password,       presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i }
+  validates :nickname,                    presence: true
+  validates :last_name,                   presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+$\z/ }
+  validates :first_name,                  presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+$\z/ }
+  validates :lastname_kana,               presence: true, format: { with: /\A([ァ-ン]|ー)+$\z/ }
+  validates :firstname_kana,              presence: true, format: { with: /\A([ァ-ン]|ー)+$\z/ }
+  validates :birth_day,                   presence: true
+  validates :email,                       presence: true, uniqueness: true, format: { with: /\A[a-zA-Z0-9_\#!$%&`'*+\-{|}~^\/=?\.]+@[a-zA-Z0-9][a-zA-Z0-9\.-]+\z/ }
+  validates :password,                    presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{7,100}+\z/i }
+  validates :password_confirmation,       presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{7,100}+\z/i }
 
   has_many :items
   has_one :address
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
   devise_scope :user do
-    get 'users', to: 'users/registrations#new'
-    post 'users', to: 'users/registrations#create'
-    get 'addresses', to: 'users/registrations#new_address'
+    get 'users',      to: 'users/registrations#new'
+    post 'users',     to: 'users/registrations#create'
+    get 'addresses',  to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
   end
   root 'items#index'
