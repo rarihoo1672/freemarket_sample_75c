2e9a1b14e75de2b6a86c5138394bea6e8298665c
hatake10969 <kawabata1096@gmail.com>
2020-05-26 14:59:19 +0900

    Implementing email authentication

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,19 +1,13 @@
-# frozen_string_literal: true
-
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # GET /resource/sign_up
   def new
     @user = User.new
-    # super
   end
 
-  # POST /resource
   def create
-  #   super
     @user = User.new(sign_up_params)
     unless @user.valid?
       flash.now[:alert] = @user.errors.full_messages
@@ -21,6 +15,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
     end
     session["devise.regist_data"] = {user: @user.attributes}
     session["devise.regist_data"][:user]["password"] = params[:user][:password]
+    session["devise.regist_data"][:user]["password_confirmation"] = params[:user][:password_confirmation]
     @address = @user.build_address
     render :new_address
   end
@@ -37,64 +32,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
       render :new_address and return
     end
     @user.build_address(@address.attributes)
-    # session["address"] = @address.attributes
     @user.save
     sign_in(:user, @user)
   end
 
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
-
-  # PUT /resource
-  # def update
-  #   super
-  # end
-
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
-
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
-
   protected
 
   def configure_permitted_parameters
-    # params.require(:user).permit(:nickname, :email, :password, :password_confirmation, :last_name, :first_name, :lastname_kana, :firstname_kana, :birth_day)
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :lastname_kana, :firstname_kana, :birth_day])
   end
 
   def address_params
     params.require(:address).permit(:last_name, :first_name, :lastname_kana, :firstname_kana, :post_code, :prefecture, :city, :address, :building_name, :tel_number)
   end
-
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
-
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
 end
