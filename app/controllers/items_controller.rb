c78dbf36236f40fa5a4be4094ea25900400c99a0
makikashiwai <mmmkpn@gmail.com>
2020-05-27 22:19:09 +0900

    一覧表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
+  before_action :index
+
   def index
+    @items = Item.on_sell.includes([:images]).order(created_at: :desc).limit(3)
   end
 
   def show
