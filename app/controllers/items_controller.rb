85b99982ebf1b5495ca2b9621fbc7191270a1c48
hatake10969 <kawabata1096@gmail.com>
2020-05-27 22:41:03 +0900

    Show category

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,9 @@ class ItemsController < ApplicationController
 
   def show
     category = Item.find(params[:id]).category_id
-    @category_name = Category.find(category).name
+    @this_category = Category.find(category)
+    @parent_category = @this_category.parent unless @this_category == nil
+    @grandparent_category = @parent_category.parent unless @parent_category == nil
   end
 
   def item_purchase
