b579bc6a5a09e399b4c3e1700e232d926365f598
hatake10969 <kawabata1096@gmail.com>
2020-05-29 17:18:29 +0900

    Create Select Box

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,6 +8,10 @@ class ItemsController < ApplicationController
     @item = Item.new
     # エラー回避のため一時的にコメントアウトしてます
     # @item.images.new
+    @category_parent_array = ["---"]
+    Category.where(ancestry: nil).each do |parent|
+       @category_parent_array << parent.name
+    end
   end
 
   def create
@@ -22,6 +26,15 @@ class ItemsController < ApplicationController
 
   def item_purchase
   end
+
+  def get_category_children
+    @category_children = Category.find_by(name: "#{params[:name]}", ancestry: nil).children
+ end
+
+ def get_category_grandchildren
+    @category_grandchildren = Category.find("#{params[:child_id]}").children
+ end
+
 
   private
 
