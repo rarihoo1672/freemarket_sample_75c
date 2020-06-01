2d9b53d495e4f518c5ff94445c35cbe384017440
makikashiwai <mmmkpn@gmail.com>
2020-06-01 12:32:10 +0900

    エラー解決

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_27_071320) do
+ActiveRecord::Schema.define(version: 2020_06_01_021207) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "last_name", null: false
@@ -18,12 +18,12 @@ ActiveRecord::Schema.define(version: 2020_05_27_071320) do
     t.string "lastname_kana", null: false
     t.string "firstname_kana", null: false
     t.string "post_code", null: false
-    t.string "prefecture", null: false
+    t.integer "prefecture_id", null: false
     t.string "city", null: false
     t.string "address", null: false
     t.string "building_name"
     t.string "tel_number"
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_addresses_on_user_id"
@@ -53,7 +53,6 @@ ActiveRecord::Schema.define(version: 2020_05_27_071320) do
     t.string "size", null: false
     t.string "shipping_cost", null: false
     t.string "shipping_days", null: false
-    t.string "prefecture_id", null: false
     t.integer "user_id", null: false
     t.integer "buyer"
     t.datetime "created_at", null: false
