be427f465d5116eb7c2824e025a5c0df35e96dbd
k1y0r4k4 <kiyoraka2209@gmail.com>
2020-06-02 12:35:48 +0900

    test追記

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,38 +3,14 @@ FactoryBot.define do
   factory :item do
     name                  {"テスト"}
     introduction          {"テストです"}
-    category              {""}？？
+    category_id           {category.id}
     size                  {"S"}
-    brand                 {"テスト"}？？
     status                {"新品、未使用"}
     sipping_cost          {"送料込み（出品者負担）"}
     prefecture_id         {1}
     sipping_days          {"1~2日で発送"}
     price                 {3000}
-    user_id               {1}
+    user_id               {user.id}
     buyer                 {""}
-
-  end
-
-  factory :user do
-    nickname              {"tanaka"}
-    email                 {"test@email.com"}
-    password              {"abc1234"}
-    password_confirmation {"abc1234"}
-    user_image            {""}
-    introduction          {""}
-    last_name             {"田中"}
-    first_name            {"太郎"}
-    lastname_kana         {"タナカ"}
-    firstname_kana        {"タロウ"}
-    birth_day             {"2000-01-01"}
-  end
-
-  factory :image do
-    image                 {"hogehoge"}
-  end
-
-  factory :category do
-    
   end
 end
