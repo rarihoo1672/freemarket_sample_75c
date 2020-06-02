0689b7da47f5e4aba23fd5b7b3be93e789724443
Ayato Seto <seto_rarihoo1672@icloud.com>
2020-06-02 18:54:10 +0900

    Add host of mailer on production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'freemarket75c@gmail.com'}
+  config.action_mailer.default_url_options = { host: Rails.application.credentials[:home_page][:ip_address]}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
