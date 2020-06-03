758d4cb8130e5d1f093b4943627b89a4b15ef34c
hatake10969 <kawabata1096@gmail.com>
2020-06-03 11:56:22 +0900

    Edit routing

--- config/routes.rb
+++ config/routes.rb
@@ -27,11 +27,20 @@ Rails.application.routes.draw do
       get "user_information"
     end
   end
+
   resources :items do
     collection do
       get "item_purchase"
       get 'get_category_children', defaults: { format: 'json' }
       get 'get_category_grandchildren', defaults: { format: 'json' }
+    end
+  end
+
+  resources :card, only: [:new, :show] do
+    collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
+      post 'delete', to: 'card#delete'
     end
   end
 end
