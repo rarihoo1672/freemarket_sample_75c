515897b94bbfc559218a1e03c4ed7f0adb9ed509
Ayato Seto <seto_rarihoo1672@icloud.com>
2020-06-04 20:40:55 +0900

    Add test of validates in comment model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "message,user_id,item_idがあれば有効" do
+    user = FactoryBot.build(:user)
+    item = FactoryBot.build(:item)
+    comment = Comment.new(
+      message: "test",
+      user_id: user.id,
+      item_id: item.id
+    )
+    expect(comment).to be_valid
+  end
+
+  it "メッセージがなければ無効" do
+    comment = Comment.new(message: nil)
+    comment.valid?
+    expect(comment.errors[:message]).to include("を入力してください")
+  end
+
+  it "user_idがなければ無効" do
+    comment = Comment.new(user_id: nil)
+    comment.valid?
+    expect(comment.errors[:user_id]).to include("を入力してください")
+  end
+
+  it "item_idがなければ無効" do
+    comment = Comment.new(item_id: nil)
+    comment.valid?
+    expect(comment.errors[:item_id]).to include("を入力してください")
+  end
 end
