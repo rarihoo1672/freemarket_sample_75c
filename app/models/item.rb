06477a499f19fb86f63a0dd67df64c9e75781fe1
Ayato Seto <seto_rarihoo1672@icloud.com>
2020-06-04 12:00:11 +0900

    Add item link method

--- app/models/item.rb
+++ app/models/item.rb
@@ -20,4 +20,13 @@ class Item < ApplicationRecord
 
   enum buyer: { sell: 0, buy: 1}
   scope :on_sell, -> { where(buyer: 0) }
+
+
+  def previous
+    Item.where("id < ?", self.id).order("id DESC").first
+  end
+
+  def next
+    Item.where("id > ?", self.id).order("id ASC").first
+  end
 end
