b19dc2d400c7d3c96d1b180db9719bc4ba3c962a
k1y0r4k4 <kiyoraka2209@gmail.com>
2020-06-05 19:47:21 +0900

    ブランド追加・修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_02_054858) do
+ActiveRecord::Schema.define(version: 2020_06_05_103911) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "last_name", null: false
@@ -60,11 +60,12 @@ ActiveRecord::Schema.define(version: 2020_06_02_054858) do
     t.string "shipping_cost", null: false
     t.string "shipping_days", null: false
     t.integer "user_id", null: false
-    t.integer "buyer"
+    t.integer "buyer", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "category_id", null: false
-    t.bigint "brand_id", null: false
+    t.bigint "brand_id"
+    t.integer "prefecture_id"
     t.index ["brand_id"], name: "index_items_on_brand_id"
     t.index ["category_id"], name: "index_items_on_category_id"
   end
