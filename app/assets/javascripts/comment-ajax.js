638a0f4b88771cfc94b65df1ee122233cbdd43c3
Ayato Seto <seto_rarihoo1672@icloud.com>
2020-06-08 20:02:43 +0900

    Improve comment ajax user image
    ユーザーが画像を持っているか？ユーザーが出品者か？どうかで条件を分けている

--- app/assets/javascripts/comment-ajax.js
+++ app/assets/javascripts/comment-ajax.js
@@ -1,29 +1,101 @@
 $(document).on('turbolinks:load', ()=> {
+  image = $('#current_user').val();
+
   function buildHTML(comment) {
-    if ( comment.user_id == comment.exhibitor_id) {
-      let html = `
-        <div class="detail-comment-box__inner comment-display">
-          <div class="detail-comment-box__inner__name comment-display__name">
-            出品者: 
-          </div>
-          <div class="detail-comment-box__inner__text comment-display__message">
-            ${comment.message}
-          </div>
-        </div>`
-      return html;
+    if ( image == "") {
+      if ( comment.user_id == comment.exhibitor_id ) {
+        let html = `
+          <div class="detail-comment-box__inner comment-display">
+            <div class="detail-comment-box__inner comment-display__comment-box exhibitor-box">
+              <div class="detail-comment-box__inner comment-display__comment-box__name">
+                <p class="exhibitor-comment">
+                  出品者
+                </p>
+              </div>
+              <div class="detail-comment-box__inner comment-display__comment-box__message">
+                <p>
+                  ${comment.message}
+                </p>
+              </div>
+            </div>
+            <div class="detail-comment-box__inner comment-display__image-box">
+              <div class="detail-comment-box__inner comment-display__image-box__exhibitor">
+                <img class="user-thumbnail" src="/assets/adidas.cap-3e39fd851cc3c67bca93be545ce2366e1ace8fdc214047c9eeb58c25b57ba1b6.jpg">
+              </div>
+            </div>
+          </div>`
+        return html;
+      } else {
+        let html = `
+          <div class="detail-comment-box__inner comment-display">
+            <div class="detail-comment-box__inner comment-display__comment-box buyer-box">
+              <div class="detail-comment-box__inner comment-display__comment-box__name">
+                <p class="user-comment">
+                  ${ comment.name }
+                </p>
+              </div>
+              <div class="detail-comment-box__inner comment-display__comment-box__message">
+                <p>
+                  ${ comment.message }
+                </p>
+              </div>
+            </div>
+            <div class="detail-comment-box__inner comment-display__image-box">
+              <div class="detail-comment-box__inner comment-display__image-box__user">
+                <img class="user-thumbnail" src="/assets/adidas.cap-3e39fd851cc3c67bca93be545ce2366e1ace8fdc214047c9eeb58c25b57ba1b6.jpg">
+              </div>
+            </div>
+          </div>`
+        return html;
+      };
     } else {
-      let html = `
-        <div class="detail-comment-box__inner comment-display">
-          <div class="detail-comment-box__inner__name comment-display__name">
-            ${comment.name}: 
-          </div>
-          <div class="detail-comment-box__inner__text comment-display__message">
-            ${comment.message}
-          </div>
-        </div>`
-      return html;
-    }
-  }
+      if ( comment.user_id == comment.exhibitor_id ) {
+        let html = `
+          <div class="detail-comment-box__inner comment-display">
+            <div class="detail-comment-box__inner comment-display__comment-box exhibitor-box">
+              <div class="detail-comment-box__inner comment-display__comment-box__name">
+                <p class="exhibitor-comment">
+                  出品者
+                </p>
+              </div>
+              <div class="detail-comment-box__inner comment-display__comment-box__message">
+                <p>
+                  ${comment.message}
+                </p>
+              </div>
+            </div>
+            <div class="detail-comment-box__inner comment-display__image-box">
+              <div class="detail-comment-box__inner comment-display__image-box__exhibitor">
+                <img class="user-thumbnail" src=${image}>
+              </div>
+            </div>
+          </div>`
+        return html;
+      } else {
+        let html = `
+          <div class="detail-comment-box__inner comment-display">
+            <div class="detail-comment-box__inner comment-display__comment-box buyer-box">
+              <div class="detail-comment-box__inner comment-display__comment-box__name">
+                <p class="user-comment">
+                  ${ comment.name }
+                </p>
+              </div>
+              <div class="detail-comment-box__inner comment-display__comment-box__message">
+                <p>
+                  ${ comment.message }
+                </p>
+              </div>
+            </div>
+            <div class="detail-comment-box__inner comment-display__image-box">
+              <div class="detail-comment-box__inner comment-display__image-box__user">
+                <img class="user-thumbnail" src=${image}>
+              </div>
+            </div>
+          </div>`
+        return html;
+      };
+    };
+  };
   $('#new_comment').on('submit', function (e) {
     e.preventDefault();
     let formData = new FormData(this);
