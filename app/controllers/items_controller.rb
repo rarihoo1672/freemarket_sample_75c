016749e2bbfb6b6d8748b49b12bd1b02868613b3
k1y0r4k4 <kiyoraka2209@gmail.com>
2020-06-08 20:15:42 +0900

    controller修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -32,10 +32,13 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    if @item.user != current_user
+      redirect_to root_path
+    end
   end
 
   def update
-    if @item.update
+    if @item.update(item_params)
       redirect_to root_path
     else
       render :edit
