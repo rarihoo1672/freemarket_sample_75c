ea303443f0f051f1b1c23bee6052eeb207daf6d1
k1y0r4k4 <kiyoraka2209@gmail.com>
2020-06-08 19:38:13 +0900

    プレビュー画像削除
    画像だけ残っていたので削除できるように追記

--- app/assets/javascripts/image-upload.js
+++ app/assets/javascripts/image-upload.js
@@ -20,18 +20,18 @@ $(document).on('turbolinks:load', ()=> {
   lastIndex = $('.img-file_group:last').data('index');
   fileIndex.splice(0, lastIndex);
 
+  $('.hidden-destroy').hide();
+
   $("#exhibit__drop-box").on('change', '.img-file', function(e) {
     const targetIndex = $(this).parent().data('index');
     const file = e.target.files[0];
     const blobUrl = window.URL.createObjectURL(file);
-    console.log(blobUrl)
 
     if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
       img.setAttribute('src', blobUrl);
     } else {
       $('#exhibit__drop-box__previews').append(buildImg(targetIndex, blobUrl));
       $("#exhibit__drop-box").append(buildFileField(fileIndex[0]));
-      console.log(targetIndex, blobUrl)
       fileIndex.shift();
       fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
     }
@@ -40,10 +40,11 @@ $(document).on('turbolinks:load', ()=> {
   $("#exhibit__drop-box").on('click','.img-remove', function() {
     const targetIndex = $(this).parent().data('index');
     const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
+    if(hiddenCheck) hiddenCheck.prop('checked', true);
 
-    if(hiddenCheck) hiddenCheck.prop('checked', true)
+    $(this).parent().remove();
+    $(`img[data-index="${targetIndex}"]`).remove();
 
-    $(this).parent().remove()
     if($(".img-file").length == 0) $("#exhibit__drop-box").append(buildFileField(fileIndex[0]));
   });
 });
