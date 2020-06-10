5f7b60c387d2f4b078699a291b2f78ad0e687296
makikashiwai <mmmkpn@gmail.com>
2020-06-10 14:21:39 +0900

    マイページ編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
 
   def show
+    @user = User.find(params[:id])
+    @sell_items = Item.where(user_id: @user.id, buyer: "sell")
+    @sold_items = Item.where(user_id: @user.id, buyer: "buy")
   end
 
   def log_out
